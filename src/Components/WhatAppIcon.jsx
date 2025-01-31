2987724b92da2795cd7a41d39720edacece4623d
remeessalam <remeesreme4u@gmail.com>
2025-01-31 16:40:00 +0530

    whatsapp numberadded

--- src/Components/WhatAppIcon.jsx
+++ src/Components/WhatAppIcon.jsx
@@ -7,13 +7,12 @@ const WhatsAppIcon = () => {
     <Link
       className="fixed group bottom-[2.5rem] hover:-translate-y-1 z-30 border border-black bg-white rounded-full right-[1.5rem] transition-all duration-200"
       // to={`https://wa.me/${clientDetails.whatsappbox}`}
-      to={`https://wa.me/${clientDetails.whatsappbox}`}
+      to={`https://api.whatsapp.com/send/?phone=%2B918591331353&text&type=phone_number&app_absent=0`}
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Contact us on WhatsApp"
     >
       {/* <WhatsApp className="w-[3rem] h-[3rem] sm:w-[4rem] sm:h-[4rem] fill-green-500 group-hover:fill-primary group-hover:scale-110 rounded-full shadow-transparent shadow-large group-hover:shadow-primary/50 transition-all duration-300" /> */}
-
       <img
         src={whatsappIcon}
         alt="WhatsApp Icon"
